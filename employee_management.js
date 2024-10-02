756cfd2210c334cba2279c82f7dae4f3c572137a
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-10-02 18:08:12 -0400

    Create Employee class with properties and methods - TASK 1

--- employee_management.js
+++ employee_management.js
@@ -1,2 +1,19 @@
 //Basic Setup
 
+//TASK ONE
+
+class Employee {
+    constructor(name, salary, position, department){
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department; 
+    } //initalization 
+  getDetails(){
+    console.log(`${this.name} works as a ${this.position} for $${this.salary}`);
+  }  //method which returnsa string of employee details
+}
+console.log("TASK ONE"); //addded for better console readability
+
+const employee1 = new Employee("Jonas", 70000, "Sales Clerk", "Sales");
+employee1.getDetails(); //for testing the class
